ba0c69576550ca80d7ab793162c6788cf95bb862
kang <rilac1@naver.com>
2022-08-29 13:38:19 +0900

    쿼리 메소드 기능 - 메소드 이름으로 쿼리 생성

--- src/main/java/study/datajpa/repository/MemberRepository.java
+++ src/main/java/study/datajpa/repository/MemberRepository.java
@@ -1,8 +1,14 @@
 package study.datajpa.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import study.datajpa.enitity.Member;
 
 public interface MemberRepository extends JpaRepository<Member, Long> {
+
+	List<Member> findByUsernameAndAgeGreaterThan(String name, int age);
+
+	List<Member> findTop3HelloBy();
 }
