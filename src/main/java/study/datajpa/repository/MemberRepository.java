5426dbfd3dda9b43135c24cd4ddf196c9e536400
kang <rilac1@naver.com>
2022-08-29 13:54:29 +0900

    쿼리 메소드 기능 - NamedQuery

--- src/main/java/study/datajpa/repository/MemberRepository.java
+++ src/main/java/study/datajpa/repository/MemberRepository.java
@@ -3,6 +3,8 @@ package study.datajpa.repository;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import study.datajpa.enitity.Member;
 
@@ -11,4 +13,7 @@ public interface MemberRepository extends JpaRepository<Member, Long> {
 	List<Member> findByUsernameAndAgeGreaterThan(String name, int age);
 
 	List<Member> findTop3HelloBy();
+
+	// @Query(name = "Member.findByUsername")
+	List<Member> findByUsername(@Param("username") String username);
 }
